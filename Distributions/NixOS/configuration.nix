a04ddc90afbf76ef16eea134677e6d61e9330aa8
ValentinMENDIss <valiksuper58@gmail.com>
2024-08-22 21:13:42 +0200

    Uploading new configuration.nix file

--- Distributions/NixOS/configuration.nix
+++ Distributions/NixOS/configuration.nix
@@ -1,161 +1,165 @@
-{ config, lib, pkgs, ... }: let
+{ config, pkgs, ... }:
 
-        flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-
-
-  in {
+{
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
+  # Sudo Editor
+  environment.variables = {
+	SUDO_EDITOR = "nvim";
+  };
 
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nix"; # Define your hostname.
-  # Pick only one of the below networking options.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-
-  nixpkgs.config.allowUnfree = true;
-
-
-
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-  # Enable the Plasma 6 Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
-
-
-
-
-
-    # other configuration still goes here
-
-
-
-
-  #  Enabling hyprlnd on NixOS
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  environment.sessionVariables = {
-    #  If your cursor becomes invisible
-    WLR_NO_HARDWARE_CURSORS = "1";
-    #  Hint electron apps to use wayland
-    NIXOS_OZONE_WL = "1";
-  };
-
-  hardware = {
-    #  Opengl
-    opengl.enable = true;
-
-    #  Most wayland compositors need this
-    nvidia.modesetting.enable = true;
-  };
-
-  # XDG portal
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-
+  # Enable the BSPWM Tiling Manager.
+  services.xserver.windowManager.bspwm.enable = true;
 
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
+
+  # Video support
+  hardware = {
+    #  Most wayland compositors need this
+    nvidia.modesetting.enable = true;
+    opengl = {
+      enable = true;
+      driSupport32Bit = true;
+      driSupport = true;
+    };
+  };
+
+  programs.zsh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mendi = {
     isNormalUser = true;
-    initialPassword = "Good_Dog13";
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    description = "mendi";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
-      firefox
-      tree
+    	neovim
+	librewolf
+	kitty
     ];
   };
 
-
-
-
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    firefox
-    neofetch
-    flameshot
-    vscode
-    htop
-    thonny
-    fish
-    lf
-    home-manager
+    # CLI Tools
+    neovim
+    yazi
+    zsh
     git
-    gimp-with-plugins
-    appflowy
-    notion-app-enhanced
-
-    python311
-    python311Packages.pip
-    vscode-extensions.ms-python.python
-    python311Packages.tkinter
-    python311Packages.pyautogui
-    python311Packages.customtkinter
-#    (python311.withPackages(ps: with ps; [ pandas requests pyautogui keyboard ]))
-
-
-    # Hyprland Packages
-    waybar
-    dunst
-    libnotify
-    swww
+    btop
     kitty
-    rofi-wayland
-    networkmanagerapplet
+    pfetch
+    uwufetch
+
+    # GUI Applications
+    librewolf
+    vlc
+
+    # BSPWM  (WM)
+    bspwm
+
+    # WM
+    polybar
+    nitrogen
+    sxhkd
+    dunst
+    picom
+    rofi
   ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "BigBlueTerminal" ]; })
+  ];
+
+
+
+  system.autoUpgrade.enable = true;
+
+  nix = {
+    # Optimising nix store
+    settings.auto-optimise-store = true;
+    # automatically collect garbage ( and delete old "snapshots")
+    gc.automatic = true;
+    gc.dates = "weekly";
+    gc.options = "--delete-older-than 30d";
+  };
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -176,28 +180,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
-

0b1adc4e3fc87146a0039fd25632842e1ca1f4e0
ValentinMENDIss <valiksuper58@gmail.com>
2024-09-16 22:47:14 +0200

    Added new plugin for Neovim (Lazyvim)

--- Tools/dotfiles/config/nvim/lua/mendi/core/keymaps.lua
+++ Tools/dotfiles/config/nvim/lua/mendi/core/keymaps.lua
@@ -25,3 +25,9 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc = "Close current tab"})
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc = "Go to next tab"}) -- go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", {desc = "Go to previous tab"}) -- go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", {desc = "Open current buffer in new tab"}) -- open current buffer in new tab
+
+
+-- plugins --
+
+-- nvimtree plugin
+keymap.set("n", "<leader>ee", "<cmd>Neotree<CR>", {desc = "Toggle file Explorer plugin"}) -- toggle file explorer

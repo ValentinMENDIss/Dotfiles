90515aacede06199abb0ec46c893ed5569d28cf7
ValentinMENDIss <valiksuper58@gmail.com>
2025-02-06 11:27:28 +0100

    resolved issues with last commit (deleted copies)

--- Tools/dotfiles/config/nvim/lua/mendi/core/init.lua
+++
@@ -1,2 +0,0 @@
-require("mendi.core.options") -- export/use options file    (main config file)
-require("mendi.core.keymaps") -- export/use keymaps config file
--- Tools/from-source/yazi/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TARGET_DIR="$HOME/Documents/Yazi"
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup update
-
-cargo install --locked yazi-fm yazi-cli
